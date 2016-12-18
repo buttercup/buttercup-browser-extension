64122763718412f6c8ab549a4bbc343123399ae6
Perry Mitchell <perry@perrymitchell.net>
2016-12-18 02:57:51 +0200

    Setup tab helper - add button to login forms

--- source/tab/index.js
+++ source/tab/index.js
@@ -1,1 +1,8 @@
-console.log("Buttercup tab");
+const FormFinder = require("./FormFinder.js");
+
+console.log("Init");
+
+let ff = new FormFinder();
+ff.findLoginForms().forEach(function(form) {
+    form.placeContextButton();
+});
--- source_old/misc/header.js
+++
@@ -1,4 +0,0 @@
-// Buttercup Chrome extension
-(function() {
-	window.require = window.lamd.require;
-	window.define = window.lamd.define;
