6332964e2f525cb3756b49731c544c0fa7065b43
Perry Mitchell <perry@perrymitchell.net>
2017-01-15 16:26:45 +0200

    Add support for browsing directory tree

--- source/setup/js/RemoteFileExplorer.jsx
+++ source/setup/js/RemoteFileExplorer.jsx
@@ -3,10 +3,21 @@
 const React = require("react");
 const Rodal = require("rodal").default;
 const Spinner = require("react-spinner");
+const Tree = require("rc-tree");
+const { TreeNode } = Tree;
 
 require("rodal/lib/rodal.css");
 require("react-spinner/react-spinner.css");
+require("rc-tree/assets/index.css");
 require("RemoteFileExplorer.sass");
+
+function processLeafData(stat) {
+    return {
+        key: stat.path,
+        name: stat.name,
+        leaf: stat.isFile()
+    };
+}
 
 class RemoteFileExplorer extends React.Component {
 
@@ -15,8 +26,7 @@ class RemoteFileExplorer extends React.Component {
         this._fsInstance = this.props.fs || null;
         this.state = {
             dirContents: null,
-            modalVisible: false,
-            remotePath: "/"
+            modalVisible: false
         };
     }
 
@@ -26,26 +36,53 @@ class RemoteFileExplorer extends React.Component {
 
     componentWillReceiveProps() {
         if (this.fs) {
-            this.fetchDirectory();
+            this.fetchDirectory("/");
         }
     }
 
-    fetchDirectory() {
-        this.fs
-            .readDirectory(this.state.remotePath)
+    fetchDirectory(dir) {
+        return this.fs
+            .readDirectory(dir)
             .then(contents => {
-                console.log("Remote contents", contents);
+                console.log("Remote contents", dir, contents);
                 this.setState({
-                    dirContents: contents
+                    dirContents: Object.assign(this.state.dirContents || {}, {
+                        [dir]: contents.map(processLeafData)
+                    })
                 });
             })
             .catch(err => {
                 alert(`An error occurred fetching Dropbox contents: ${err.message}`);
+                throw err;
             });
     }
 
     hide() {
         this.setState({ modalVisible: false });
+    }
+
+    onLoadData(treeNode) {
+        let targetPath = treeNode.props.eventKey;
+        if (this.state.dirContents[targetPath]) {
+            return Promise.resolve();
+        } else {
+            return this
+                .fetchDirectory(targetPath)
+                .then(() => {
+                    // this.setState({
+                    //     dirContents: Object.assign(this.state.dirContents, {
+                    //         [targetPath]: Object.assign(this.state.dirContents[targetPath], {
+                    //             children: true
+                    //         })
+                    //     })
+                    // });
+                    // console.log("SET STATE", targetPath, this.state);
+                })
+                .catch(function(err) {
+                    alert(`Failed loading data for path: ${targetPath}`);
+                    throw err;
+                });
+        }
     }
 
     render() {
@@ -56,15 +93,44 @@ class RemoteFileExplorer extends React.Component {
                     <div className="modalContents">
                         {this.state.dirContents === null ?
                             <Spinner /> :
-                            <div>
-                                <pre>
-                                    {JSON.stringify(this.state.dirContents, undefined, 4)}
-                                </pre>
-                            </div>
+                            this.renderTree()
                         }
                     </div>
                 </Rodal>
             </div>
+        );
+    }
+
+    renderTree() {
+        const loopItem = (filePath) => {
+            let items = this.state.dirContents[filePath];
+            return items.map(item => {
+                if (this.state.dirContents[item.key]) {
+                    return (
+                        <TreeNode
+                            title={item.name}
+                            key={item.key}
+                            >
+                            {loopItem(item.key)}
+                        </TreeNode>
+                    );
+                }
+                return (
+                    <TreeNode
+                        title={item.name}
+                        key={item.key}
+                        isLeaf={item.leaf}
+                        />
+                );
+            });
+        };
+        const treeNodes = loopItem("/");
+        return (
+            <Tree
+                loadData={(node) => this.onLoadData(node)}
+                >
+                {treeNodes}
+            </Tree>
         );
     }
 
