2b03e6a317137c407453efe4cd5b1cfed1c46d1d
Perry Mitchell <perry@perrymitchell.net>
2017-01-28 13:01:47 +0200

    Hide about page (for now)

--- source/setup/js/HeaderBar.jsx
+++ source/setup/js/HeaderBar.jsx
@@ -13,6 +13,7 @@ class HeaderBar extends React.Component {
                 <ul className="menu">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/addArchive">Add archive</Link></li>
+                    {/* <li><Link to="/about">About</Link></li> */}
                 </ul>
             </div>
         );
