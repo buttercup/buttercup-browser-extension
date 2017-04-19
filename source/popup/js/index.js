85acc479edf0d08bd3216b4a0b36748117670dc1
Perry Mitchell <perry@perrymitchell.net>
2017-04-19 22:20:29 +0300

    Increase timeout for locking archives

--- source/popup/js/index.js
+++ source/popup/js/index.js
@@ -14,7 +14,7 @@ window.Buttercup = {
 
     lockArchive: function(name) {
         return new Promise(function(resolve, reject) {
-            let timeout = setTimeout(() => reject(new Error("Timed-out locking archive")), 200);
+            let timeout = setTimeout(() => reject(new Error("Timed-out locking archive")), 4000);
             chrome.runtime.sendMessage({ command: "lock-archive", name }, function(response) {
                 clearTimeout(timeout);
                 if (response && response.ok) {
