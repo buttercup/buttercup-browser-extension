f8d4cab6e3c0b2b78c3c2a2bfec623177c882741
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-10-14 23:04:16 +0300

    Fix navigation

--- source/shared/containers/App.js
+++ source/shared/containers/App.js
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import { getConfigKey } from "../selectors/app.js";
 import App from "../components/App.js";
 
-export default connect(
-    (state, ownProps) => ({
-        darkMode: getConfigKey(state, "darkMode")
-    }),
-    {}
-)(App);
+export default withRouter(
+    connect(
+        (state, ownProps) => ({
+            darkMode: getConfigKey(state, "darkMode")
+        }),
+        {}
+    )(App)
+);
