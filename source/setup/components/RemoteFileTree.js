6d5d82befb763938645dd333de54bd95e70c9572
Murray Urreta <murreta@protonmail.com>
2018-12-03 08:19:39 -0300

    Fix loading validation issue of files tree

--- source/setup/components/RemoteFileTree.js
+++ source/setup/components/RemoteFileTree.js
@@ -231,7 +231,7 @@ class RemoteFileTree extends Component {
     render() {
         return (
             <Choose>
-                <When condition={this.props.rootDirectory && this.props.rootDirectory.directories.length > 0}>
+                <When condition={this.props.rootDirectory && this.props.rootDirectory.files.length > 0}>
                     <Tree
                         contents={this.getTree(this.props.rootDirectory).childNodes}
                         onNodeExpand={::this.handleNodeExpand}
