2570d02e98ce686384d047e8cd312694b9fa4d42
Perry Mitchell <perry@perrymitchell.net>
2019-07-23 15:40:35 +0300

    Add unlock-all-vaults button to dialog when no sources unlocked

--- source/dialog/components/SearchPage.js
+++ source/dialog/components/SearchPage.js
@@ -1,15 +1,18 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { NonIdealState } from "@blueprintjs/core";
+import { Button, NonIdealState } from "@blueprintjs/core";
 import styled from "styled-components";
 import SearchBar from "../containers/SearchBar.js";
 import SearchResults from "../containers/SearchResults.js";
 import DialogFrame from "./DialogFrame.js";
 
+const BUTTERCUP_LOGO = require("../../../resources/buttercup-standalone.png");
+
 class SearchPage extends PureComponent {
     static propTypes = {
         availableSources: PropTypes.number.isRequired,
-        onPrepareFirstResults: PropTypes.func.isRequired
+        onPrepareFirstResults: PropTypes.func.isRequired,
+        onUnlockAllArchives: PropTypes.func.isRequired
     };
 
     componentWillMount() {
@@ -26,9 +29,16 @@ class SearchPage extends PureComponent {
                     </When>
                     <Otherwise>
                         <NonIdealState
-                            icon="lock"
                             title="No unlocked vaults"
-                            description="No vaults are currently available or unlocked. Why not get started by adding one?"
+                            description="No vaults are currently available or unlocked."
+                            icon={<img src={BUTTERCUP_LOGO} width="64" />}
+                            action={
+                                this.props.availableSources === 0 ? (
+                                    <Button icon="unlock" onClick={::this.props.onUnlockAllArchives}>
+                                        Unlock Vaults
+                                    </Button>
+                                ) : null
+                            }
                         />
                     </Otherwise>
                 </Choose>
