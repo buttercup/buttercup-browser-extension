6e68000a0f3ee34243bcb1a55148e90b75f6e533
Perry Mitchell <perry@perrymitchell.net>
2023-02-15 20:03:41 +0200

    Add toaster

--- source/shared/services/notifications.ts
+++ source/shared/services/notifications.ts
@@ -1,3 +1,9 @@
-import { Toaster } from "@blueprintjs/core";
+import { Position, Toaster, ToasterInstance } from "@blueprintjs/core";
 
-const __toaster = Toaster.create({});
+const __toaster = Toaster.create({
+    position: Position.BOTTOM_RIGHT
+});
+
+export function getToaster(): ToasterInstance {
+    return __toaster;
+}
